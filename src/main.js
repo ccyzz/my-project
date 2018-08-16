937003c5ff452823ee280ac0d917d0fd652769ce
ccyzz <1280249202@qq.com>
2018-08-16 22:37:31 +0800

    封装jsonp方法

--- src/main.js
+++ src/main.js
@@ -7,11 +7,26 @@ import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import './assets/iconfont/iconfont.css'
 import './assets/css/style.css'
+import jsonp from 'jsonp'
 import axios from 'axios'
+// import { resolve } from 'url'; 不知道
 // import MyAxios from '@/plugins/MyAxios'
 
 Vue.prototype.$ajax = axios
 Vue.config.productionTip = false
+
+// axios扩展的jsonp方法
+axios.jsonp = (...args) => {
+  return new Promise((resolve, reject) => {
+    jsonp(...args, function (err, data) {
+      if(err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
 
 // 注册element-ui插件
 Vue.use(ElementUI)
